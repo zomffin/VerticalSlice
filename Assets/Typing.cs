f9ecc0f16ba4d4977908b2d80409747a40ca1371
zom <matsunoisabel@gmail.com>
2026-04-21 21:28:13 -0700

    scrapping the grabbing paper thing...

--- Assets/Typing.cs
+++ Assets/Typing.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Transactions;
 using TMPro;
 using UnityEditorInternal;
 using UnityEngine;
@@ -52,30 +53,37 @@ public class Typing : MonoBehaviour
         _taskPassage = task;
     }
 
-    private void OnTriggerEnter(Collider other)
+    //everyhing in game dev is hard and im crying 
+    
+    /*private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("on trigger enter triggered");
+        
         if (other.name.Contains("Paper"))
         {
             _paper = other.GetComponentInChildren<TextMeshPro>(); 
             _currentPassage =  _paper.text;
             other.transform.position = _position.position;
             other.transform.rotation = _position.rotation;
-            other.GetComponent<Rigidbody>().isKinematic = false;
+            //other.GetComponent<Rigidbody>().isKinematic = false;
+            other.GetComponent<Rigidbody>(). = false; 
             _player.TakeItem();
         }
         else
         {
             Debug.Log("hit with a collider that isnt paper");
         }
-    }
+    }*/
 
-    private void OnTriggerExit(Collider other)
+    /*private void OnTriggerExit(Collider other)
     {
+        Debug.Log("On trigger exit triggered");
+        
         if (other.name.Contains("Paper"))
         {
             _paper = null;
             _currentPassage = "";
-            other.GetComponent<Rigidbody>().isKinematic = true;
+            //other.GetComponent<Rigidbody>().isKinematic = true;
         }
-    }
+    }*/
 }
