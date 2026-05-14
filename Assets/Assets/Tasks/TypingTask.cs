9a9331a86323e9391672b5457f79fd21162a4bb8
zom <matsunoisabel@gmail.com>
2026-05-13 22:44:12 -0700

    Add new tasks, adjust paper prefab (to fit more characters)

--- Assets/Assets/Tasks/TypingTask.cs
+++ Assets/Assets/Tasks/TypingTask.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TypingTask", menuName = "Scriptable Objects/Tasks/Typing Task")]
 public class TypingTask : Task
 {
+    public bool hasSpecialInstruction;
+    public string specialInstruction; 
+    
     [TextArea(15,20)]
     public string displayText; 
 
